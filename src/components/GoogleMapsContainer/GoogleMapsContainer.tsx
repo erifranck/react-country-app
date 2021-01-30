6e7292bb7977b732e015121522bd43386e6736a0
erifranck-nunez-ch <erickfrank@goldmediatech.com>
2021-01-29 22:23:22 -0500

    add google maps

---
+++ src/components/GoogleMapsContainer/GoogleMapsContainer.tsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
+import './GoogleMapsContainer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkedAlt, faMapSigns, faMarker } from '@fortawesome/free-solid-svg-icons';
+ 
+interface Props  {
+    position: {
+        lat: number;
+        lng: number;
+    }
+}
+
+// set an api key to show google map
+const GOOGLE_API = 'AIzaSyAk5rMVliEsdzRhRhLLC-jSBn2zQcyqvbE';
+
+interface MarkerProps {
+  lat: number;
+  lng: number;
+}
+
+const Marker: React.FC<MarkerProps> = () => (
+  <FontAwesomeIcon className="map-marker" icon={faMapMarkedAlt}/>
+);
+
+export class MapContainer extends Component<Props> {
+  render() {
+    return (
+     <div className="map-wrapper">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: GOOGLE_API }}
+          defaultCenter={this.props.position}
+          defaultZoom={5}
+        >
+          <Marker
+            lat={this.props.position.lat}
+            lng={this.props.position.lng}
+          />
+        </GoogleMapReact>
+      </div> 
+    );
+  }
+}
